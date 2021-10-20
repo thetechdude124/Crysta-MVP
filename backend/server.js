2160908e69082e94d69c3152ca14bfb7aa27e8bd
Aditya Dewan <65571552+thetechdude124@users.noreply.github.com>
2021-10-20 13:45:20 -0400

    Updated paths in server.js

--- backend/server.js
+++ backend/server.js
@@ -52,10 +52,10 @@ app.post('/send-user', (req, res) => {
 
 // });
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join('frontend', 'build')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join('frontend', 'build', 'index.html'));
 });
 
 console.log("app/frontend") 
