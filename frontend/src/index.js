26d0df592a0ad75cd458f772680df643a3bfa936
thetechdude124 <aditya.dewan124@gmail.com>
2021-12-06 10:04:25 -0500

    --- Auto-Git Commit ---

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Login from './Login';
 import { Auth0Provider } from "@auth0/auth0-react";
 
 
